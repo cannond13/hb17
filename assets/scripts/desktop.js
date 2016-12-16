13f511dfde4ecfa9889cd89c402d6c59662161bd
Dac Cannon <dac.cannon@gmail.com>
2016-12-16 11:54:35 -0500

    assets files for formatting

---
+++ assets/scripts/desktop.js
@@ -0,0 +1,4 @@
+if (screen.width >= 500) window.location = "https://bldg.co/home/desktop";
+
+  // if (screen.width >= 500) window.location.replace("https://bldg.co")
+  // else window.location.replace("http://www.example.com/index.html")
